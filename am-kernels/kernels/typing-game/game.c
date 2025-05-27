77cfa4dfe07293ad5b16020dafe6bd793917f8d1
tracer-ysyx <tracer@ysyx.org>
2025-05-27 07:35:05 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:35:05 up 56 min,  1 user,  load average: 0.52, 0.30, 0.25

--- am-kernels/kernels/typing-game/game.c
+++ am-kernels/kernels/typing-game/game.c
@@ -3,7 +3,7 @@
 #include <klib-macros.h>
 
 #define FPS 30
-#define CPS 5
+#define CPS 1
 #define CHAR_W 8
 #define CHAR_H 16
 #define NCHAR 128
@@ -48,7 +48,7 @@ void new_char()
 			c->ch = 'A' + randint(0, 25);
 			c->x = randint(0, screen_w - CHAR_W);
 			c->y = 0;
-			c->v = (screen_h - CHAR_H + 1) / randint(FPS * 6, FPS * 4);
+			c->v = (screen_h - CHAR_H + 1) / randint(FPS * 3 / 2, FPS * 2);
 			c->t = 0;
 			return;
 		}
