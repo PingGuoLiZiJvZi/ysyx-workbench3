999e253feddaae0df6ae9b3a3c10c65dd19e2061
tracer-ysyx <tracer@ysyx.org>
2025-07-03 01:33:40 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:33:40 up 21:31,  1 user,  load average: 1.09, 0.62, 0.29

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,21 +2,33 @@
 #include <elf.h>
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
-
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+static char program_buf[0x100000];
+extern size_t get_ramdisk_size(void);
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+	Log("Loading program '%s'...", filename);
+	uint32_t size = get_ramdisk_size();
+	if (size >= sizeof(program_buf))
+	{
+		panic("Program size exceeds buffer size");
+	}
+	ramdisk_read(program_buf, 0, size);
+	Elf_Ehdr *ehdr = (Elf_Ehdr *)program_buf;
+	assert(*(uint32_t *)ehdr->e_ident == 0x464c457f); // Check ELF magic number
+	return ehdr->e_entry;							  // Return the entry point address
+	return 0;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
-  ((void(*)())entry) ();
+void naive_uload(PCB *pcb, const char *filename)
+{
+	uintptr_t entry = loader(pcb, filename);
+	Log("Jump to entry = %p", entry);
+	((void (*)())entry)();
 }
-
