731b18f364af101c0aab2d9fd520ba3d2cf94893
tracer-ysyx <tracer@ysyx.org>
2025-07-04 04:33:39 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:33:39 up 2 days, 31 min,  1 user,  load average: 0.17, 0.16, 0.27

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -25,10 +25,55 @@ int SDL_WaitEvent(SDL_Event *event)
 	if (buf[1] == 'd')
 	{
 		event->type = SDL_KEYDOWN;
-		// 只封装 j/down k/up g
+		switch (buf[3])
+		{
+		case '0':
+			event->key.keysym.sym = SDLK_0;
+			break;
+		case '1':
+			event->key.keysym.sym = SDLK_1;
+			break;
+		case '2':
+			event->key.keysym.sym = SDLK_2;
+			break;
+		case '3':
+			event->key.keysym.sym = SDLK_3;
+			break;
+		case '4':
+			event->key.keysym.sym = SDLK_4;
+			break;
+		case '5':
+			event->key.keysym.sym = SDLK_5;
+			break;
+		case '6':
+			event->key.keysym.sym = SDLK_6;
+			break;
+		case '7':
+			event->key.keysym.sym = SDLK_7;
+			break;
+		case '8':
+			event->key.keysym.sym = SDLK_8;
+			break;
+		case '9':
+			event->key.keysym.sym = SDLK_9;
+			break;
+		case 'G':
+			event->key.keysym.sym = SDLK_G;
+			break;
+		case 'J':
+			event->key.keysym.sym = SDLK_J;
+			break;
+		case 'K':
+			event->key.keysym.sym = SDLK_K;
+			break;
+		default:
+			event->key.keysym.sym = SDLK_NONE;
+			break;
+		}
 	}
 	else
 		event->type = SDL_KEYUP;
+	return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
