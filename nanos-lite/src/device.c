fad803f29daf056e4bdee22e30c92caecbf7160d
tracer-ysyx <tracer@ysyx.org>
2025-07-03 23:08:44 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:08:44 up 1 day, 19:06,  1 user,  load average: 0.16, 0.48, 0.29

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -51,7 +51,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len)
 	{
 		panic("dispinfo_read: buffer too small");
 	}
-	printf("dispinfo_read: %s", p);
+	printf("dispinfo_read: %s\n", p);
 	memcpy(buf, p, n);
 	return 0;
 }
