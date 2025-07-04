77c13b53bf7fdd0089fcb6fc2da16a4679a53cd7
tracer-ysyx <tracer@ysyx.org>
2025-07-04 04:30:47 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:30:47 up 2 days, 28 min,  1 user,  load average: 0.14, 0.15, 0.29

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -4,26 +4,39 @@
 #define keyname(k) #k,
 
 static const char *keyname[] = {
-  "NONE",
-  _KEYS(keyname)
-};
+	"NONE",
+	_KEYS(keyname)};
 
-int SDL_PushEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PushEvent(SDL_Event *ev)
+{
+	return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+int SDL_PollEvent(SDL_Event *ev)
+{
+	return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
-  return 1;
+int SDL_WaitEvent(SDL_Event *event)
+{
+	char buf[64] = {0};
+
+	NDL_PollEvent(buf, sizeof(buf) - 1);
+	if (buf[1] == 'd')
+	{
+		event->type = SDL_KEYDOWN;
+		// 只封装 j/down k/up g
+	}
+	else
+		event->type = SDL_KEYUP;
 }
 
-int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  return 0;
+int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask)
+{
+	return 0;
 }
 
-uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+uint8_t *SDL_GetKeyState(int *numkeys)
+{
+	return NULL;
 }
