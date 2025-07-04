b8499175d9aa5272bb4a750afe55c5f4dd9515cf
tracer-ysyx <tracer@ysyx.org>
2025-07-03 21:30:50 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:30:50 up 1 day, 17:28,  1 user,  load average: 0.04, 0.03, 0.05

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -34,19 +34,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len)
 	return 0;
 }
 
-size_t output_write(const void *buf, size_t offset, size_t len)
-{
-	for (size_t i = 0; i < len; i++)
-	{
-		putch(((const char *)buf)[i]);
-	}
-	return len;
-}
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
 	[FD_STDIN] = {"stdin", 0, 0, invalid_read, invalid_write},
-	[FD_STDOUT] = {"stdout", 0, 0, invalid_read, output_write},
-	[FD_STDERR] = {"stderr", 0, 0, invalid_read, output_write},
+	[FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+	[FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
