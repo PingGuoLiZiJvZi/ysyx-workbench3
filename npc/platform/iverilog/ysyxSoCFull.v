d1eec183f9df9fa44d5e9a1319e4a41b59ce43ec
tracer-ysyx <tracer@ysyx.org>
2025-08-07 04:48:36 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:48:36 up  8:25,  1 user,  load average: 0.00, 0.03, 0.09

--- npc/platform/iverilog/ysyxSoCFull.v
+++ npc/platform/iverilog/ysyxSoCFull.v
@@ -8,6 +8,8 @@ module ysyxSoCFull (
 
 always #1 clock = ~clock;
 initial begin
+	$dumpfile("top.vcd");
+    $dumpvars(0,ysyxSoCFull);
 	$readmemh("build/program.hex", flash_mem);
 	$display("ysyxSoCFull: flash memory loaded");
 	repeat(10) @(posedge clock);
