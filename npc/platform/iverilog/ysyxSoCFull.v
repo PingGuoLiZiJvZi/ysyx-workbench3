176a48d907c39b0d5f8346c870ec4c0fae91168c
tracer-ysyx <tracer@ysyx.org>
2025-08-07 05:58:56 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:58:56 up  9:35,  1 user,  load average: 0.07, 0.29, 0.35

--- npc/platform/iverilog/ysyxSoCFull.v
+++ npc/platform/iverilog/ysyxSoCFull.v
@@ -230,8 +230,8 @@ always @(posedge clock) begin
 				sdram_mem[(write_addr_store - `ysyx_25040129_SDRAM_ADDR)>> 2] <= (write_data_store & strb)|
 					(sdram_mem[(write_addr_store - `ysyx_25040129_SDRAM_ADDR)>> 2] & ~strb);
 				`ifdef ysyx_25040129_SDRAM_WRITE_DISPLAY
-				$display("ysyxSoCFull: write sdram addr %h, data %h, write index %h", write_addr_store, sdram_mem[(write_addr_store -
-					`ysyx_25040129_SDRAM_ADDR)>> 2], (write_addr_store - `ysyx_25040129_SDRAM_ADDR)>> 2);
+				$display("ysyxSoCFull: write sdram addr %h, data %h, write index %h", write_addr_store,(write_data_store & strb)|
+					(sdram_mem[(write_addr_store - `ysyx_25040129_SDRAM_ADDR)>> 2] & ~strb); , (write_addr_store - `ysyx_25040129_SDRAM_ADDR)>> 2);
 				`endif
 			end else if(write_addr_store >= `ysyx_25040129_UART_REG_ADDR && write_addr_store < `ysyx_25040129_UART_REG_ADDR + `ysyx_25040129_UART_REG_SIZE) begin
 				$write("%c", write_data_store[7:0]);
