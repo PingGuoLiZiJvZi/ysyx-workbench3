6e70488739e48fa5ec9157c7467a785c1779fcd3
tracer-ysyx <tracer@ysyx.org>
2025-08-07 23:14:23 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:23 up 1 day,  2:51,  1 user,  load average: 0.10, 0.14, 0.34

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -13,7 +13,6 @@ static Area segments[] = { // Kernel memory mappings
 
 static inline void set_satp(void *pdir)
 {
-	printf("Setting SATP with page directory %p\n", pdir);
 	uintptr_t mode = 1ul << (__riscv_xlen - 1);
 	asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
 }
