ade3c7d89064e5214641c89dbbc45bb77f3730eb
tracer-ysyx <tracer@ysyx.org>
2025-08-07 22:26:06 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:06 up 1 day,  2:03,  1 user,  load average: 0.17, 0.14, 0.15

--- npc/vsrc/ysyx_25040129_IDU.v
+++ npc/vsrc/ysyx_25040129_IDU.v
@@ -153,7 +153,7 @@
 	assign funct7_5 = inst[30];
 	assign funct3 = inst[14:12];
 	assign opcode = inst[6:0];
-	assign rd_out_idu = inst[10:7];
+	assign rd_out_idu = inst[11:7];
 	assign src1_id = inst[19:15];
 	assign src2_id = inst[24:20];
 //---------------调试信号---------------
