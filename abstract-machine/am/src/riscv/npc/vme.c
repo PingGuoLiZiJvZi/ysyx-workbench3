043a67b3490af06844124634c75ffebbf0745dcf
tracer-ysyx <tracer@ysyx.org>
2025-08-09 10:34:24 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:24 up 1 day,  5:15,  1 user,  load average: 0.55, 0.56, 0.35

--- abstract-machine/am/src/riscv/npc/vme.c
+++ abstract-machine/am/src/riscv/npc/vme.c
@@ -16,6 +16,7 @@ static inline void set_satp(void *pdir)
 {
 	uintptr_t mode = 1ul << (__riscv_xlen - 1);
 	asm volatile("csrw satp, %0" : : "r"(mode | ((uintptr_t)pdir >> 12)));
+	printf("Setting satp to %p\n", pdir);
 }
 
 static inline uintptr_t get_satp()
