088a1425ed79eaca908b6876a5656d3c014a0a67
tracer-ysyx <tracer@ysyx.org>
2025-08-10 03:00:17 -0400

    > sim RTL ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  03:00:17 up  5:38,  1 user,  load average: 0.01, 0.08, 0.13

--- am-kernels/tests/cpu-tests/tests/csrrw.c
+++ am-kernels/tests/cpu-tests/tests/csrrw.c
@@ -2,4 +2,33 @@
 
 int main()
 {
+	unsigned int csr_value;
+	unsigned int new_value = 0x114;
+	unsigned int reg_value = 0x514;
+
+	unsigned int csr_address = 0x300;
+
+	__asm__ __volatile__(
+		"csrw %[csr], %[new]\n\t"
+		:
+		: [csr] "i"(csr_address), [new] "r"(new_value));
+
+	// 使用内联汇编读取 CSR 寄存器的当前值
+	__asm__ __volatile__(
+		"csrr %[old], %[csr]\n\t"
+		: [old] "=r"(csr_value)
+		: [csr] "i"(csr_address));
+
+	// 使用内联汇编将 CSR 寄存器的值与通用寄存器的值互换
+	__asm__ __volatile__(
+		"csrrw %[reg], %[csr], %[reg]\n\t"
+		: [reg] "+r"(reg_value)
+		: [csr] "i"(csr_address));
+
+	// 输出 CSR 的原始值、新值和通用寄存器的值
+	printf("Original CSR value: 0x%08x\n", csr_value);
+	printf("New CSR value: 0x%08x\n", new_value);
+	printf("Register value after swap: 0x%08x\n", reg_value);
+
+	return 0;
 }
--- npc/vsrc/ysyx_25040129_IDU.v
+++ npc/vsrc/ysyx_25040129_IDU.v
@@ -302,8 +302,6 @@
 						src1_out_idu = csr_in_idu; 
 						src2_out_idu= 32'b0; 
 						alu_opcode = `ysyx_25040129_ADD; 
-						if(rd_out_idu != 0)$error("IDU: do not support CSR write with rd != 0 now!");
-						
 					end
 					3'b010:begin
 						is_csrr = 1'b1;
