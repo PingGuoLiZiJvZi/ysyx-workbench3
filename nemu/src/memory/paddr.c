2f89b1232ec6a1a3ba81202c67b7af4e9642c0a4
tracer-ysyx <tracer@ysyx.org>
2025-09-01 02:00:48 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:00:48 up 1 min,  1 user,  load average: 2.48, 1.22, 0.47

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,7 @@ void init_mem()
 	pmem = malloc(CONFIG_MSIZE);
 	assert(pmem);
 #endif
-	// IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
+	IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
 	Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
