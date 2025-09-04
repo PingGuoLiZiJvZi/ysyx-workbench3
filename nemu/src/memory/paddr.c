7610d0a3ebeada56f7254844242007d7768d8b5a
tracer-ysyx <tracer@ysyx.org>
2025-09-04 05:43:41 -0400

    > compile NEMU ysyx_25040129 周陈曦 Linux pglzjz-virtual-machine 6.8.0-78-generic #78~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 13 14:32:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:43:41 up  5:21,  1 user,  load average: 0.16, 0.18, 0.17

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -44,7 +44,26 @@ static void out_of_bound(paddr_t addr)
 	panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
 		  addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
-
+void xv6_make_ramdisk()
+{
+	// if(is_set) return;
+	// is_set = 1;
+	FILE *fp = fopen("fs.img", "rb");
+	if (!fp)
+	{
+		printf("Can not open fs.img\n");
+		return;
+	}
+	// 把fs.img加载到内存的0x86000000处
+	fseek(fp, 0, SEEK_END);
+	size_t size = ftell(fp);
+	assert(size < 512 * 1024);
+	fseek(fp, 0, SEEK_SET);
+	int ret = fread(guest_to_host(0x86000000), size, 1, fp);
+	assert(ret == 1);
+	fclose(fp);
+	printf("Load fs.img to memory, size = %lu\n", size);
+}
 void init_mem()
 {
 #if defined(CONFIG_PMEM_MALLOC)
@@ -52,6 +71,7 @@ void init_mem()
 	assert(pmem);
 #endif
 	IFDEF(CONFIG_MEM_RANDOM, memset(pmem, rand(), CONFIG_MSIZE));
+	xv6_make_ramdisk();
 	Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
 
